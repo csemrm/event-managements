c2b36b2862f45503dd609e4a576356570c159e69
Mostafizur Rahman <csemrm@gmail.com>
2023-11-12 11:48:12 -0800

    loadRelationships

--- app/Http/Controllers/Api/AttendeeController.php
+++ app/Http/Controllers/Api/AttendeeController.php
@@ -4,22 +4,27 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\AttendeeResource;
+use App\Http\Traits\CanLoadRelationships;
 use App\Models\Attendee;
 use App\Models\Event;
 use Illuminate\Http\Request;
 
 class AttendeeController extends Controller
 {
+    use CanLoadRelationships;
+    private array $relations = ['user'];
     /**
      * Display a listing of the resource.
      */
     public function index( Event $event )
     {
-        $attendees = $event->attendees()->latest();
+
+        $attendees =$this->loadRelationships(
+            $event->attendees()->latest()
+        );
 
         return AttendeeResource::collection(
-            $attendees->load('user')
-            ->paginate(10)
+            $attendees->paginate(10)
         );
     }
 
@@ -33,7 +38,7 @@ class AttendeeController extends Controller
             "event_id"=>$event->id
         ] );
 
-        return new AttendeeResource($attendee);
+        return new AttendeeResource($this->loadRelationships($attendee));
     }
 
     /**
@@ -41,7 +46,7 @@ class AttendeeController extends Controller
      */
     public function show( Event $event, Attendee $attendee )
     {
-        return new AttendeeResource($attendee);
+        return new AttendeeResource($this->loadRelationships($attendee));
     }
 
     /**
--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -4,42 +4,29 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\EventResource;
+use App\Http\Traits\CanLoadRelationships;
 use App\Models\Event;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
+    use CanLoadRelationships;
+    private  array $relations = ['user', 'attendees', 'attendees.user'];
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
 
-        $query = Event::query();
-        $relations = ['user', 'attendees', 'attendees.user'];
-        foreach ($relations as $relation) {
-            $query->when(
-                $this->shouldIncludeRelation($relation),
-                fn($q) => $q->with($relation)
-            );
-        }
+
+        $query =  $this->loadRelationships(Event::query());
+
 
         return EventResource::collection(
            $query->latest()
             ->paginate());
     }
 
-    protected function shouldIncludeRelation(string $relation){
-        $include = request()->query('include');
-        if (!$include) {
-            return false;
-        }
-
-
-        $relations = array_map('trim', explode(',', $include));
-
-        return in_array($relation, $relations);
-    }
     /**
      * Store a newly created resource in storage.
      */
@@ -54,8 +41,8 @@ class EventController extends Controller
             ]),
             'user_id' =>1
         ]);
-        $event = $event->load('user','attendees');
-        return  new EventResource( $event);
+
+        return  new EventResource($this->loadRelationships($event));
     }
 
     /**
@@ -63,8 +50,8 @@ class EventController extends Controller
      */
     public function show(Event $event)
     {
-        $event = $event->load('user','attendees');
-        return  new EventResource($event);
+
+        return  new EventResource($this->loadRelationships($event));
 
     }
 
@@ -73,18 +60,17 @@ class EventController extends Controller
      */
     public function update(Request $request, Event $event)
     {
-    $event->update([
-        ...$request->validate([
-            "name"=> "sometimes|string|max:255",
-            "description"=> "nullable|string",
-            "start_time"=> "sometimes|date",
-            "end_time"=> "sometimes:date|after:start_time"
-            ]),
-            'user_id' =>1
-        ]);
+        $event->update([
+            ...$request->validate([
+                "name"=> "sometimes|string|max:255",
+                "description"=> "nullable|string",
+                "start_time"=> "sometimes|date",
+                "end_time"=> "sometimes:date|after:start_time"
+                ]),
+                'user_id' =>1
+            ]);
 
-    $event = $event->load('user', 'attendees');
-    return new EventResource($event);
+        return new EventResource($this->loadRelationships($event));
     }
 
     /**
